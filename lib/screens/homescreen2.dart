e2193c9656e275f16b5489c079c2aa12e9359b7f
Shantanu Sawant <shanpantech264@gmail.com>
2025-03-20 12:37:23 +0530

    leaning git

---
+++ lib/screens/homescreen2.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Homescreen2 extends StatelessWidget {
+  const Homescreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
